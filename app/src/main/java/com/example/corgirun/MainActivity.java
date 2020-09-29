69a74de5c417a3786079ca8d18cbc3b0ada9c914
Cheshiriks <vlad-20-11-94@yandex.ru>
2020-09-30 02:49:30 +0300

    Create project

---
+++ app/src/main/java/com/example/corgirun/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.corgirun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.puzzle.LoopPuz;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LoopPuz loopPuz = new LoopPuz();
+        loopPuz.startGame();
+    }
+}
