0d9bad8468d88dfa7e6f210fb685765fd38a9996
Cheshiriks <vlad-20-11-94@yandex.ru>
2020-10-27 01:06:40 +0300

    0.5.4 - generate Enemies.

--- app/src/main/java/com/example/corgirun/clases/GameManager.java
+++ app/src/main/java/com/example/corgirun/clases/GameManager.java
@@ -10,6 +10,8 @@ import com.example.corgirun.objects.Picture;
 import com.example.corgirun.objects.Witch;
 import com.example.puzzle.CorePuz;
 import com.example.puzzle.GraphicsPuz;
+import com.example.puzzle.ObjectPuz;
+import java.util.ArrayList;
 
 public class GameManager {
 
@@ -21,8 +23,16 @@ public class GameManager {
 	private double distance;
 
 	Corgi corgi;
+
 	Witch witch;
+	Witch witch2;
 	Ghost ghost;
+	Ghost ghost2;
+	Witch witch3;
+	Witch witch4;
+	Ghost ghost3;
+	Ghost ghost4;
+
 	Background background_1;
 	Background background_2;
 	Fireplace fireplace;
@@ -31,6 +41,9 @@ public class GameManager {
 
 	ButtonPause buttonPause;
 
+	ArrayList<ObjectPuz> enemiesList = new ArrayList<>();
+	ObjectPuz lastEnemy;
+
 	public GameManager(CorePuz corePuz, int sceneWidth, int sceneHeight) {
 		this.maxScreenX = sceneWidth;
 		this.maxScreenY = sceneHeight;
@@ -38,8 +51,16 @@ public class GameManager {
 		this.minScreenY = 0;
 		distance = 0;
 		corgi = new Corgi(corePuz, maxScreenX, maxScreenY, minScreenY);
-		witch = new Witch(1.5, 0.1, maxScreenX, 60, maxScreenX, maxScreenY);
-		ghost = new Ghost(1.5, 0.1, maxScreenX+(maxScreenX/2), 80, maxScreenX, maxScreenY);
+
+		witch = new Witch(0, 0.1, 2*maxScreenX, 60, maxScreenX, maxScreenY);
+		witch2 = new Witch(0, 0.1, 2*maxScreenX, 60, maxScreenX, maxScreenY);
+		ghost = new Ghost(0, 0.1, 2*maxScreenX, 80, maxScreenX, maxScreenY);
+		ghost2 = new Ghost(0, 0.1, 2*maxScreenX, 80, maxScreenX, maxScreenY);
+		witch3 = new Witch(0, 0.1, 2*maxScreenX, 60, maxScreenX, maxScreenY);
+		witch4 = new Witch(0, 0.1, 2*maxScreenX, 60, maxScreenX, maxScreenY);
+		ghost3 = new Ghost(0, 0.1, 2*maxScreenX, 80, maxScreenX, maxScreenY);
+		ghost4 = new Ghost(0, 0.1, 2*maxScreenX, 80, maxScreenX, maxScreenY);
+
 		background_1 = new Background(1, 0, 0, maxScreenX, maxScreenY);
 		background_2 = new Background(1, background_1.getWeight(), 0, maxScreenX, maxScreenY);
 		fireplace = new Fireplace(1, 144, 0, maxScreenX, maxScreenY);
@@ -47,6 +68,18 @@ public class GameManager {
 		fire = new Fire(1, 432, 0, maxScreenX, maxScreenY);
 
 		buttonPause = new ButtonPause(corePuz, 0, 0);
+
+		enemiesList.add(witch);
+		enemiesList.add(ghost);
+		enemiesList.add(witch2);
+		enemiesList.add(ghost2);
+		enemiesList.add(witch3);
+		enemiesList.add(ghost3);
+		enemiesList.add(witch4);
+		enemiesList.add(ghost4);
+
+		lastEnemy = enemiesList.get(0);
+		lastEnemy.setSpeed(1.5);
 	}
 
 	public void update() {
@@ -59,10 +92,19 @@ public class GameManager {
 		buttonPause.update();
 
 		witch.update();
+		witch2.update();
 		ghost.update();
+		ghost2.update();
+		witch3.update();
+		witch4.update();
+		ghost3.update();
+		ghost4.update();
+
 		corgi.update();
 
 		distance += background_1.getSpeed() / 10;
+
+		generateEnemies(enemiesList);
 	}
 
 	public void drawing(CorePuz corePuz, GraphicsPuz graphicsPuz) {
@@ -75,7 +117,14 @@ public class GameManager {
 		buttonPause.drawing(graphicsPuz);
 
 		witch.drawing(graphicsPuz);
+		witch2.drawing(graphicsPuz);
 		ghost.drawing(graphicsPuz);
+		ghost2.drawing(graphicsPuz);
+		witch3.drawing(graphicsPuz);
+		witch4.drawing(graphicsPuz);
+		ghost3.drawing(graphicsPuz);
+		ghost4.drawing(graphicsPuz);
+
 		corgi.drawing(graphicsPuz);
 	}
 
@@ -85,6 +134,30 @@ public class GameManager {
 
 	public double getDistance() {
 		return distance;
+	}
+
+	private void generateEnemies(ArrayList<ObjectPuz> enemiesList) {
+
+		if (lastEnemy.getX() < 2*maxScreenX - randomDistance()) {
+
+			ArrayList<ObjectPuz> enemiesList2 = new ArrayList<>();
+			for (ObjectPuz enemy : enemiesList) {
+				if (enemy.getX() == 2 * maxScreenX) {
+					enemiesList2.add(enemy);
+				}
+			}
+
+			lastEnemy = enemiesList2.get(randomEnemy(enemiesList2.size() - 1));
+			lastEnemy.setSpeed(1.5);
+		}
+	}
+
+	private int randomDistance() {
+		return 140 + (int) (Math.random() * 100);
+	}
+
+	private int randomEnemy(int max) {
+		return (int) (Math.random()*max);
 	}
 
 }
